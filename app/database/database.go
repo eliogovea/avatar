d7e7dccd43d80b72606b443fc146e9d0e368bc99
Elio Govea <eliogovea1993@gmail.com>
2019-11-16 10:19:55 -0500

    reset

--- app/database/database.go
+++
@@ -1,6 +0,0 @@
-package database
-
-type DB struct {
-	Admins  adminsDAO
-	Avatars avatarsDAO
-}
--- app/model/admin.go
+++
@@ -1,9 +0,0 @@
-package model
-
-import "gopkg.in/mgo.v2/bson"
-
-type Admin struct {
-	ID         bson.ObjectId `bson:"_id"`
-	Username   string        `bson:"username" json:"username"`
-	ApprovedBy string        `bson:"approved_by" json:"approved_by"`
-}
